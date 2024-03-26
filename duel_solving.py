Fixed rotation problem on better cube

duel_solving.py
@@ -49,42 +49,21 @@ class better_cube:
         self.ring_matrix = [side_ring_in, forward_ring_in]
         self.top_value = self.ring_matrix[1][2]
 
-     def get_index(self, value):
-        if value > 0:
-            return (value %  4)
-        else:
-            return (value % -4)
-
-
      def roll (self, shift, isForward):
-        ring_copy = self.ring_matrix.copy()[isForward]
+        ring_copy = self.ring_matrix[isForward].copy()
         ring_real = self.ring_matrix[isForward]
-        for i in range(len(ring_real)):
-            ring_real[i] = ring_copy[self.get_index(i+1)]
+        if shift % 4 != 0:
+            for i in range(len(ring_real)):
+                index_unwrapped = i+shift
+                index_wrapped = index_unwrapped % 4 if index_unwrapped > 0 else index_unwrapped % -4
+                ring_real[i] = ring_copy[index_wrapped]
         print(ring_real)
         self.top_value = ring_real[2]
-
-'''
-     def roll (self, shift, isForward):
-#        shift %= 4 if  shift > 0 else -4 #Module operators with negative numbers are weird
-        for i in range(shift):
-            matrix_copy = self.ring_matrix.copy()
-            for i in range(len(self.ring_matrix[isForward])):
-                index_to_put = self.get_index(i+1)
-                self.ring_matrix[isForward][i] = matrix_copy[isForward][index_to_put]
-            self.ring_matrix = matrix_copy
-        self.top_value = self.ring_matrix[isForward][2]
-'''
 
 class game:
     def __init__(self):
         game_matrix = np.zeros((9, 8))
 
-best_cube = better_cube([6, 4, 1, 3], [6, 2, 1, 5])
-best_cube.roll(1, 1)
-best_cube.roll(1, 1)
-best_cube.roll(1, 1)
-best_cube.roll(1, 1)
-best_cube.roll(1, 1)
-best_cube.roll(1, 1)
+best_cube = better_cube(side_ring_in=[6, 4, 1, 3], forward_ring_in=[6, 2, 1, 5])
+best_cube.roll(shift=1, isForward=0)
 print(best_cube.top_value)