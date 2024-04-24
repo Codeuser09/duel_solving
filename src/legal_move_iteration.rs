68ad340d5293b64a096eebfa21912be5f6ef87bb
simon <simon.schober@outlook.de>
2024-04-24 08:59:51 +0200

    Wrote the function for discarding possible but legal moves

--- src/legal_move_iteration.rs
+++ src/legal_move_iteration.rs
@@ -1,12 +1,31 @@
-use crate::cube::{MoveArray};
-use crate::libcube::get_top;
+use crate::cube::{make_move, MoveArray};
 use crate::game::{Board, InfoMatrix};
+use crate::libcube::get_top;
 
 // move_array = [cube_id, forward_fields, turn_direction]
 
-fn discard_legal_moves (board: &Board, info_matrix: &InfoMatrix, possible_moves: Vec<MoveArray>) {}
+fn discard_legal_moves(
+    board: &Board,
+    info_matrix: InfoMatrix,
+    possible_moves: &mut Vec<MoveArray>,
+    is_white: &bool,
+) {
+    for (i, mut possible_move) in possible_moves.iter_mut().enumerate() {
+        let mut original_board = board.clone();
+        let mut original_info_matrix: InfoMatrix = info_matrix.clone();
+        if make_move(
+            &mut original_board,
+            &mut original_info_matrix,
+            is_white,
+            &mut possible_move,
+        ) == 1
+        {
+            possible_moves.remove(i);
+        }
+    }
+}
 
-pub fn get_legal_moves (board: &Board, info_matrix: &InfoMatrix) -> Vec<MoveArray> {
+pub fn get_legal_moves(board: &Board, info_matrix: &InfoMatrix, is_white: &bool) -> Vec<MoveArray> {
     let mut legal_moves = vec![];
     let mut possible_turn_directions = [0, 1, 2, 3];
     let mut possible_is_sw = [0, 1];
@@ -20,8 +39,14 @@ pub fn get_legal_moves (board: &Board, info_matrix: &InfoMatrix) -> Vec<MoveArra
         }
         for possible_turn_direction in possible_turn_directions {
             for possible_forward_field in &possible_forward_fields {
-                for is_sw in possible_is_sw { //Be careful with this, as we should probably have duplicates with 0 ff and td
-                    legal_moves.push([cube_id as i32, *possible_forward_field, possible_turn_direction, is_sw])
+                for is_sw in possible_is_sw {
+                    //Be careful with this, as we should probably have duplicates with 0 ff and td
+                    legal_moves.push([
+                        cube_id as i32,
+                        *possible_forward_field,
+                        possible_turn_direction,
+                        is_sw,
+                    ])
                 }
             }
         }
