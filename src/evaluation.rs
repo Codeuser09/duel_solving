91d0e8f5820fd417c406d3a0df555654ea2e7c25
Simon <simon.schober@outlook.de>
2024-04-29 16:12:46 +0200

    Added top_value_total evaluation

--- src/evaluation.rs
+++ src/evaluation.rs
@@ -1,6 +1,6 @@
 use crate::game::{Board, InfoMatrix};
 use crate::legal_move_iteration::get_legal_moves;
-use crate::libcube::get_smallest_unit;
+use crate::libcube::{get_smallest_unit, get_top};
 
 fn cube_amount_evaluation (info_matrix: &InfoMatrix) -> f64 {
     let mut w_cube_amount = 0f64;
@@ -76,15 +76,38 @@ fn winning_square_distance (info_matrix: &InfoMatrix) -> f64 {
     return winning_square_distance.powf(winning_square_distance) * get_smallest_unit(&(winning_square_distance as i32)) as f64 //Inverted because a smaller distance is good
 }
 
+pub fn top_value_total (board: &Board, info_matrix: &InfoMatrix) -> f64 {
+    let mut white_total = 0;
+    let mut black_total = 0;
+
+    for cube in info_matrix {
+        let cube_pos_x: usize = cube[0] as usize;
+        let cube_pos_y: usize = cube[1] as usize;
+        if cube[3] == 1 {
+            white_total += get_top(&board[cube_pos_x][cube_pos_y]);
+        }
+        else {
+            black_total += get_top(&board[cube_pos_x][cube_pos_y]);
+        }
+    }
+    return white_total as f64 - black_total as f64;
+}
+
+pub fn legal_move_total (board: &Board, info_matrix: &InfoMatrix) -> f64 {
+    return (get_legal_moves(&board, &info_matrix, true).len() as f64 - get_legal_moves(&board, &info_matrix, false).len() as f64) / 100f64;
+}
+
 pub fn evaluate_position (board: &Board, info_matrix: &InfoMatrix) -> f64 {
 
     // The evaluation is always from white's perspective, so a positive score indicates a white advantage
 
     let mut evaluation= 0f64;
     evaluation += cube_amount_evaluation(info_matrix);
-    evaluation = evaluation + (get_legal_moves(&board, &info_matrix, true).len() as f64 - get_legal_moves(&board, &info_matrix, false).len() as f64) / 100f64;
     evaluation += winning_square_distance(&info_matrix);
-    // println!("Winning square distance: {}", winning_square_distance(&info_matrix));
+    evaluation += legal_move_total(&board, &info_matrix);
+    // evaluation += top_value_total(&board, &info_matrix);
+
+    println!("Top value total: {}", top_value_total(&board, &info_matrix));
 
     return evaluation
 }
